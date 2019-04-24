f6ea15da2363978702bf65c98cccf639398d562e
mirandaa <mirandaa@mpi-cbg.de>
2019-04-24 10:02:29 +0200

    formatting

--- new_mfql_parser/mfql_Parser_v2.py
+++ new_mfql_parser/mfql_Parser_v2.py
@@ -594,7 +594,7 @@ if __name__ == '__main__':
         FA1M = FA2.mass; 
         FA1C = "%d" % "((FA2.chemsc)[C])";
         FA1DB = "%d" % "((FA2.chemsc)[db] - 1.5)"; 
-            FA1ERR = "%2.2f" % "(FA2.errppm)";
+        FA1ERR = "%2.2f" % "(FA2.errppm)";
         FA1I = FA2.intensity;
 
         FA2M = FA1.mass; 
