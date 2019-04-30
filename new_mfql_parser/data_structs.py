c94df15219c6e71eb76b31ea25f9e24a500ee1d1
mirandaa <mirandaa@mpi-cbg.de>
2019-04-30 15:56:06 +0200

    ref: change from str to txt because namespace

--- new_mfql_parser/data_structs.py
+++ new_mfql_parser/data_structs.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 mfql_dict = {}
 Var = namedtuple('Var', 'id object Options')
 Obj = namedtuple('Obj', 'p_rule p_values')
-ElementSeq = namedtuple('ElementSeq', 'str')
+ElementSeq = namedtuple('ElementSeq', 'txt')
 Evaluable = namedtuple('Evaluable', 'p_method , p_values')
 ReportItem = namedtuple('ReportItem', 'id p_values')
+
