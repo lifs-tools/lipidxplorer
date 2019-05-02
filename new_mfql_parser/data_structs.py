6e50183253b8a82b77f72b38491a91d8054e3900
mirandaa <mirandaa@mpi-cbg.de>
2019-05-02 15:22:43 +0200

    feat: add function type

--- new_mfql_parser/data_structs.py
+++ new_mfql_parser/data_structs.py
@@ -3,7 +3,8 @@ from collections import namedtuple
 mfql_dict = {}
 Var = namedtuple('Var', 'id object Options')
 Obj = namedtuple('Obj', 'p_rule p_values')
+Func = namedtuple('Func', 'func on')
 ElementSeq = namedtuple('ElementSeq', 'txt')
-Evaluable = namedtuple('Evaluable', 'operation , term_1, term_2')
+Evaluable = namedtuple('Evaluable', 'operation term_1 term_2')
 ReportItem = namedtuple('ReportItem', 'id p_values')
 
--- new_mfql_parser/suchthat2df.py
+++ new_mfql_parser/suchthat2df.py
@@ -1,5 +1,14 @@
-from data_structs import Obj, ElementSeq, Evaluable
+import warnings
 
+from data_structs import Obj, ElementSeq, Evaluable, Func
+
+
+def funcCall(func):
+    # todo make this better from the parser
+    operation = func.func
+    warnings.warn('only evaluates first item in list', DeprecationWarning)
+    on = evaluate(func.on[0])
+    return 'do {} on {}'.format(operation, on)
 
 def getObj(object):
     return str(object)
@@ -14,13 +23,14 @@ def evaluate(evaluable):
         return getObj(evaluable)
     elif isinstance(evaluable, ElementSeq):
         return ElementSeq2df(evaluable)
-
-    # process like swagger, see python_mztab
-    print('>>>>')
-    print(evaluable.operation)
-    print(evaluate(evaluable.term_1))
-    print(evaluate(evaluable.term_2))
-    print('<<<<')
+    elif isinstance(evaluable, Func):
+        return funcCall(evaluable)
+    elif isinstance(evaluable, Evaluable):
+        print('>>>>')
+        print(evaluable.operation)
+        print(evaluate(evaluable.term_1))
+        print(evaluate(evaluable.term_2))
+        print('<<<<')
 
     # eval() # use this to evaluate???
     res_df = None  # result is a dataframe with the evaluated results
@@ -32,48 +42,30 @@ def suchthat2df(suchthat):
 
 
 if __name__ == '__main__':
-    suchthat = Evaluable(operation='AND', term_1=Evaluable(operation='AND', term_1=Obj(p_rule='p_onlyObj_function2',
-                                                                                       p_values=['isOdd', '(', [Obj(
-                                                                                           p_rule='p_withAttr_accessItem_',
-                                                                                           p_values=['pr', '.',
-                                                                                                     'chemsc', '[', 'H',
-                                                                                                     ']'])], ')']),
-                                                           term_2=Obj(p_rule='p_onlyObj_function2',
-                                                                      p_values=['isOdd', '(', [Evaluable(operation='*',
-                                                                                                         term_1=Obj(
-                                                                                                             p_rule='p_withAttr_accessItem_',
-                                                                                                             p_values=[
-                                                                                                                 'pr',
-                                                                                                                 '.',
-                                                                                                                 'chemsc',
-                                                                                                                 '[',
-                                                                                                                 'db',
-                                                                                                                 ']']),
-                                                                                                         term_2=2)],
-                                                                                ')'])), term_2=Evaluable(operation='==',
-                                                                                                         term_1=Evaluable(
-                                                                                                             operation='+',
-                                                                                                             term_1=Evaluable(
-                                                                                                                 operation='+',
-                                                                                                                 term_1=Obj(
-                                                                                                                     p_rule='p_withAttr_id',
-                                                                                                                     p_values=[
-                                                                                                                         'FA1',
-                                                                                                                         '.',
-                                                                                                                         'chemsc']),
-                                                                                                                 term_2=Obj(
-                                                                                                                     p_rule='p_withAttr_id',
-                                                                                                                     p_values=[
-                                                                                                                         'FA2',
-                                                                                                                         '.',
-                                                                                                                         'chemsc'])),
-                                                                                                             term_2=ElementSeq(
-                                                                                                                 txt='C9 H19 N1 O6 P1')),
-                                                                                                         term_2=Obj(
-                                                                                                             p_rule='p_withAttr_id',
-                                                                                                             p_values=[
-                                                                                                                 'pr',
-                                                                                                                 '.',
-                                                                                                                 'chemsc'])))
+    suchthat = Evaluable(operation='AND', term_1=Evaluable(operation='AND', term_1=Func(func='isOdd', on=[
+        Obj(p_rule='p_withAttr_accessItem_', p_values=['pr', '.', 'chemsc', '[', 'H', ']'])]), term_2=Func(func='isOdd',
+                                                                                                           on=[
+                                                                                                               Evaluable(
+                                                                                                                   operation='*',
+                                                                                                                   term_1=Obj(
+                                                                                                                       p_rule='p_withAttr_accessItem_',
+                                                                                                                       p_values=[
+                                                                                                                           'pr',
+                                                                                                                           '.',
+                                                                                                                           'chemsc',
+                                                                                                                           '[',
+                                                                                                                           'db',
+                                                                                                                           ']']),
+                                                                                                                   term_2=2)])),
+                         term_2=Evaluable(operation='==', term_1=Evaluable(operation='+',
+                                                                           term_1=Evaluable(operation='+', term_1=Obj(
+                                                                               p_rule='p_withAttr_id',
+                                                                               p_values=['FA1', '.', 'chemsc']),
+                                                                                            term_2=Obj(
+                                                                                                p_rule='p_withAttr_id',
+                                                                                                p_values=['FA2', '.',
+                                                                                                          'chemsc'])),
+                                                                           term_2=ElementSeq(txt='C9 H19 N1 O6 P1')),
+                                          term_2=Obj(p_rule='p_withAttr_id', p_values=['pr', '.', 'chemsc'])))
     res = suchthat2df(suchthat)
     print(res)
