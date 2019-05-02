9376505cbea6d011d323c838436ce6c29451a1b5
mirandaa <mirandaa@mpi-cbg.de>
2019-05-02 09:59:15 +0200

    fix: typo

--- new_mfql_parser/data_structs.py
+++ new_mfql_parser/data_structs.py
@@ -4,6 +4,6 @@ mfql_dict = {}
 Var = namedtuple('Var', 'id object Options')
 Obj = namedtuple('Obj', 'p_rule p_values')
 ElementSeq = namedtuple('ElementSeq', 'txt')
-Evaluable = namedtuple('Evaluable', 'operation , term_1 term_2')
+Evaluable = namedtuple('Evaluable', 'operation , term_1, term_2')
 ReportItem = namedtuple('ReportItem', 'id p_values')
 
