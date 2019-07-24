1994010b04d0c48f4006a3534b08246e9a754450
mirandaa <mirandaa@mpi-cbg.de>
2019-07-24 09:41:42 +0200

    merged from fadi isas, what is cleanely imlemented

--- setupLX.py
+++ setupLX.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
 	name = "LipidXplorer",
-	version = "1.2.7",
-	author = "Ronny Herzog",
-	author_email = "herzog@mpi-cbg.de",
-	url = "https://wiki.mpi-cbg.de/wiki/lipidx/index.php/Main_Page",
+	version = "1.2.8",
+	author = "Ronny Herzog, Fadi Al Machot, Jacobo Miranda Ackerman",
+	author_email = "lifs-support@isas.de",
+	url = "https://lifs.isas.de/wiki/index.php/Main_Page",
 	packages = ['lx'],# 'mfql'],
 	package_data = {'lx' : ['lx/*']},#, 'mfql' : ['mfql/*']},
 	windows = [
