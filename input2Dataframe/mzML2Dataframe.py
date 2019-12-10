386de0f6448bc606a76bfd000a2346a02f940ba1
mirandaa <mirandaa@mpi-cbg.de>
2019-12-10 13:19:06 +0100

    doc: bins bay be a fraction of root of no samples

--- input2Dataframe/mzML2Dataframe.py
+++ input2Dataframe/mzML2Dataframe.py
@@ -2,11 +2,33 @@ import logging, os
 import numpy as np
 import pandas as pd
 log = logging.getLogger(os.path.basename(__file__))
-from pyteomics import mzml, auxiliary # TODO use pymzml with python 3.x
+from pyteomics import mzml, auxiliary
+import pymzml
+
+import pymzml
+
+def mzML2DataFrames_faster(filename): #this is with pytheomics
+    idx_list = [] #TODO this should be a np.array
+    fs_list = []
+    i_list = []
+    m_list = []
+    msrun = pymzml.run.Reader(file_path)
+    for spectrum in msrun:
+        idx = spectrum['id']
+        fs  = spectrum['MS:1000512']
+        i   = spectrum.mz
+        m   =  spectrum.i
+
+            #Append to content
+            count = len(i)
+            idx_list.extend([idx]*count)
+            fs_list.extend([fs]*count)
+            i_list.extend(i)
+            m_list.extend(m)
+    return pd.DataFrame({'scanNum':idx_list, 'filterLine':fs_list, 'intensity':i_list, 'mass':m_list})
 
 
-
-def mzML2DataFrames(filename):
+def mzML2DataFrames(filename): #this is with pytheomics
     idx_list = [] #TODO this should be a np.array
     fs_list = []
     i_list = []
