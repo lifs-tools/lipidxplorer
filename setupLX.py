8f7d85f0e0815dbe930a453192606842c33abcb3
Nils Hoffmann <nils.hoffmann@isas.de>
2019-12-11 10:03:21 +0100

    Updated version.

--- setupLX.py
+++ setupLX.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name = "LipidXplorer",
-	version = "1.2.8",
+	version = "1.2.8.1",
 	author = "Ronny Herzog, Jacobo Miranda Ackerman, Fadi Al Machot, Nils Hoffmann",
 	author_email = "lifs-support@isas.de",
 	url = "https://lifs.isas.de/wiki/index.php/Main_Page",
