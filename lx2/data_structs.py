60968ac63836c6acca06ba6c027510ccc9cf59c7
mirandaa <mirandaa@mpi-cbg.de>
2020-02-11 19:36:14 +0100

    break: testing the repsort part

--- lx2/data_structs.py
+++ lx2/data_structs.py
@@ -6,7 +6,7 @@ Obj = namedtuple('Obj', 'p_rule p_values')
 Func = namedtuple('Func', 'func on')
 ElementSeq = namedtuple('ElementSeq', 'txt')
 Evaluable = namedtuple('Evaluable', 'operation term_1 term_2')
-ReportItem = namedtuple('ReportItem', 'id p_values isTuple')
+ReportItem = namedtuple('ReportItem', 'id p_values is_PCT_format')
 ReportCol = namedtuple('ReportCol', 'col_name col_fortmat, col_eval_txt')
 
 
