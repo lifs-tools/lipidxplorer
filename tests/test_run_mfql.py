c64147482d4591fc36f48b718a82d3efa80c0e0d
mirandaa <mirandaa@mpi-cbg.de>
2021-08-24 11:28:47 +0200

    test: makle it faster

--- tests/test_run_mfql.py
+++ tests/test_run_mfql.py
@@ -5,11 +5,12 @@ import pickle
 from utils import read_options, proy_path, getMfqlFiles, make_MFQL_result, makeResultsString, out_reference
 
 def test_run_mfql():
-    # proy = r'test_resources\small_test\small_test-project.lxp'
     options = read_options(proy_path)
     with open(options['masterScanFileRun'],'rb') as handle:
         masterscan = pickle.load(handle)
     masterscan.listSurveyEntry = [se for se in masterscan.listSurveyEntry if 600< se.peakMean <750]
+    for se in masterscan.listSurveyEntry:
+        se.listMSMS = [msms for msms in se.listMSMS if 360 < msms.mass < 380]
     mfqlFiles = getMfqlFiles(r'test_resources\small_test')
     result = make_MFQL_result(masterscan, mfqlFiles, options)
     resultStr = makeResultsString(result, options)
