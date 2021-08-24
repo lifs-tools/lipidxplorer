e6038c45444ce76e7a6d1772c2cc88bf39dc2142
mirandaa <mirandaa@mpi-cbg.de>
2021-08-24 11:18:34 +0200

    test: fix now it runs

--- tests/test_run_mfql.py
+++ tests/test_run_mfql.py
@@ -2,17 +2,16 @@ import pytest
 from pathlib import Path
 import pickle
 
-from utils import read_options, proy_path, getMfqlFiles, make_MFQL_result, makeResultsString
+from utils import read_options, proy_path, getMfqlFiles, make_MFQL_result, makeResultsString, out_reference
 
 def test_run_mfql():
+    # proy = r'test_resources\small_test\small_test-project.lxp'
     options = read_options(proy_path)
-    # masterscan = make_masterscan(options)
     with open(options['masterScanFileRun'],'rb') as handle:
         masterscan = pickle.load(handle)
+    masterscan.listSurveyEntry = [se for se in masterscan.listSurveyEntry if 600< se.peakMean <750]
     mfqlFiles = getMfqlFiles(r'test_resources\small_test')
     result = make_MFQL_result(masterscan, mfqlFiles, options)
     resultStr = makeResultsString(result, options)
-    reference = Path(r'..\test_resources\small_test\small_test-out.csv').read_text()
-    print(f'are same {resultStr == reference}')
-
-    assert resultStr == reference
+    
+    assert resultStr.strip() == out_reference.strip()
