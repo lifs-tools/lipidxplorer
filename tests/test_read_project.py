73e11e247e838d4ae5c338ebb6985d49e032a773
mirandaa <mirandaa@mpi-cbg.de>
2021-09-24 17:39:05 +0200

    feat: if msresolition is 0 or auto it will run lx2, also pep8 formatting

--- tests/test_read_project.py
+++ tests/test_read_project.py
@@ -7,3 +7,11 @@ def test_read_project():
     options = read_options(proy_path)
 
     assert expected_options_str == str(options)
+
+
+def test_trigger_calcres():
+    options = read_options(proy_path, make_msresolution_auto=True)
+    # assert options["MSresolution"] == "auto"
+    assert options["alignmentMethodMS"] == "calctol"
+    assert options["alignmentMethodMSMS"] == "calctol"
+    assert options["scanAveragingMethod"] == "calctol"
--- tests/utils.py
+++ tests/utils.py
@@ -8,54 +8,58 @@ from lx.mfql.runtimeExecution import TypeMFQL
 from lx.mfql.mfqlParser import startParsing
 
 
-
-proy_path = r'test_resources\small_test\small_test-project.lxp'
+proy_path = r"test_resources\small_test\small_test-project.lxp"
 expected_options_str = r"""{'setting': 1.0, 'selectionWindow': 0.4, 'MStolerance': 2.00 ppm, 'MSMStolerance': 2.00 ppm, 'MSresolution': 5.00 ppm, 'MSMSresolution': 11.11 ppm, 'MSresolutionDelta': -100.0, 'MSMSresolutionDelta': -90.0, 'MSthreshold': 0.0001, 'MSMSthreshold': 0.0001, 'MSminOccupation': 0.5, 'MSMSminOccupation': 0.5, 'precursorMassShift': 0.0, 'precursorMassShiftOrbi': 0.0, 'optionalMStolerance': 5.00 ppm, 'optionalMSMStolerance': 5.00 ppm, 'MSfilter': 0.6, 'MSMSfilter': 0.6, 'loopNr': 3, 'importMSMS': True, 'pisSpectra': False, 'isotopicCorrection_MSMS': False, 'removeIsotopes': True, 'isotopesInMasterScan': False, 'monoisotopicCorrection': False, 'relativeIntensity': False, 'logMemory': False, 'intensityCorrection': False, 'masterScanInSQL': False, 'sumFattyAcids': False, 'isotopicCorrectionMS': True, 'isotopicCorrectionMSMS': True, 'complementMasterScan': False, 'noHead': False, 'compress': False, 'tabLimited': False, 'dumpMasterScan': False, 'statistics': False, 'noPermutations': True, 'settingsPrefix': False, 'timerange': (33.0, 1080.0), 'MScalibration': [680.4802], 'MSmassrange': (360.0, 1000.0), 'MSMSmassrange': (150.0, 1000.0), 'MStoleranceType': 'ppm', 'MSMStoleranceType': 'ppm', 'optionalMStoleranceType': 'ppm', 'optionalMSMStoleranceType': 'ppm', 'alignmentMethodMS': 'linear', 'alignmentMethodMSMS': 'linear', 'scanAveragingMethod': 'linear', 'masterScanFileImport': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\small_test.sc', 'masterScanFileRun': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\small_test.sc', 'dumpMasterScanFile': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\small_test-dump.csv', 'complementMasterScanFile': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\small_test-complement.csv', 'importDir': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test', 'masterScanRun': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\small_test.sc', 'masterScanImport': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\small_test.sc', 'dataType': 'mzML', 'ini': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\lpdxImportSettings_benchmark.ini', 'MSMScalibration': '', 'MSthresholdType': 'absolute', 'MSMSthresholdType': 'absolute', 'intensityCorrectionPrecursor': '', 'intensityCorrectionFragment': '', 'resultFile': 'D:\\fork\\isas_lipidxplorer\\test_resources\\small_test\\small_test-out.csv', 'optionalMSthreshold': 'None', 'optionalMSMSthreshold': 'None', 'optionalMSthresholdType': 'None', 'optionalMSMSthresholdType': 'None', 'mzXML': 'None', 'spectraFormat': 'mzML'}"""
 
-expected_ms_path = r'test_resources\small_test\small_test_LX12.sc' 
+expected_ms_path = r"test_resources\small_test\small_test_LX12.sc"
 
-def read_options(project_path):
+
+def read_options(project_path, make_msresolution_auto=False):
+
     project = Project()
     project.load(project_path)
+    if make_msresolution_auto:
+        project.options["MSresolution"] = "auto"
     project.testOptions()
     project.formatOptions()
     options = project.getOptions()
     return options
 
 
-#==========masterscan
+# ==========masterscan
 def make_masterscan(options):
-    listIntermission = lpdxImportDEF_new(
-			options = options,
-			parent = None)
+    listIntermission = lpdxImportDEF_new(options=options, parent=None)
 
-    scan = doImport(listIntermission[0],
-			listIntermission[1],
-			listIntermission[2],
-			listIntermission[3],
-			listIntermission[4],
-			listIntermission[5],
-			listIntermission[6],
-			listIntermission[7],
-			options['alignmentMethodMS'],
-			options['alignmentMethodMSMS'],
-			options['scanAveragingMethod'],
-			options['importMSMS'])
+    scan = doImport(
+        listIntermission[0],
+        listIntermission[1],
+        listIntermission[2],
+        listIntermission[3],
+        listIntermission[4],
+        listIntermission[5],
+        listIntermission[6],
+        listIntermission[7],
+        options["alignmentMethodMS"],
+        options["alignmentMethodMSMS"],
+        options["scanAveragingMethod"],
+        options["importMSMS"],
+    )
     return scan
 
 
-def  compareMasterScans(created, reference):
+def compareMasterScans(created, reference):
     c_ls = created.listSurveyEntry
     a_ls = reference.listSurveyEntry
 
-    same =True
-    for c,a in zip(c_ls, a_ls):
+    same = True
+    for c, a in zip(c_ls, a_ls):
         if c != a:
             same = False
             break
     return same
-    
-#================= mfql
+
+
+# ================= mfql
 
 out_reference = """SPECIE,CLASS,MASS,IDMSLEVEL,QUANTMSLEVEL,ISOBARIC,CHEMSC,ERRppm,FRERRppm,INT:190321_Serum_Lipidextract_368723_01.mzML,INT:190321_Serum_Lipidextract_368723_02.mzML,QUALA:190321_Serum_Lipidextract_368723_01.mzML,QUALA:190321_Serum_Lipidextract_368723_02.mzML,QUALB:190321_Serum_Lipidextract_368723_01.mzML,QUALB:190321_Serum_Lipidextract_368723_02.mzML,QUALC:190321_Serum_Lipidextract_368723_01.mzML,QUALC:190321_Serum_Lipidextract_368723_02.mzML
 
@@ -83,50 +87,54 @@ CE 22:6,CE,714.6192762222361,2.0,2.0,,C49 H80 O2 N1 ,1.29,0.32,191528.7,195760.5
 CE 22:5,CE,716.6347799842056,2.0,2.0,,C49 H82 O2 N1 ,1.08,0.48,32964.7,34786.1,61850.1,48246.2,61850.1,48246.2,61850.1,48246.2
 """
 
+
 def getMfqlFiles(root_mfql_dir):
     p = Path(root_mfql_dir)
-    mfqls = p.glob('*.mfql')
-    return {mfql.name:mfql.read_text() for mfql in mfqls }
+    mfqls = p.glob("*.mfql")
+    return {mfql.name: mfql.read_text() for mfql in mfqls}
+
 
 def makeResultsString(result, options):
     # result = mfqlObj.result
-    outputSeperator = ','
-    #as in lxmain
+    outputSeperator = ","
+    # as in lxmain
     if result.mfqlOutput:
-        strHead = ''
-        if not options['noHead']:
+        strHead = ""
+        if not options["noHead"]:
             for key in result.listHead:
                 if key != result.listHead[-1]:
-                    strHead += key + '%s' % outputSeperator
+                    strHead += key + "%s" % outputSeperator
                 else:
                     strHead += key
 
             # generate whole string
             strResult = "%s\n" % strHead
         else:
-            strResult = ''
+            strResult = ""
 
         for k in result.dictQuery.values():
             strResult += "\n###,%s\n" % k.name
             strResult += k.strOutput
-            strResult += '\n'
+            strResult += "\n"
 
     return strResult
+
 
 def make_MFQL_result(masterscan, mfqlFiles, options):
     mfqlObj = TypeMFQL(masterScan=masterscan)
     mfqlObj.options = options
-    mfqlObj.outputSeperator = ','
+    mfqlObj.outputSeperator = ","
 
-    (progressCount, returnValue) = startParsing(mfqlFiles,
-            mfqlObj,
-            masterscan,
-            isotopicCorrectionMS = options['isotopicCorrectionMS'],
-            isotopicCorrectionMSMS = options['isotopicCorrectionMSMS'],
-            complementSC = options['complementMasterScan'],
-            parent = None,
-            progressCount = 0,
-            generateStatistics = options['statistics'],
-            )
+    (progressCount, returnValue) = startParsing(
+        mfqlFiles,
+        mfqlObj,
+        masterscan,
+        isotopicCorrectionMS=options["isotopicCorrectionMS"],
+        isotopicCorrectionMSMS=options["isotopicCorrectionMSMS"],
+        complementSC=options["complementMasterScan"],
+        parent=None,
+        progressCount=0,
+        generateStatistics=options["statistics"],
+    )
 
     return mfqlObj.result
