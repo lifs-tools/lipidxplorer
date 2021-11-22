8b7ef87cb669c1eadde78df8158c756fbcaa6ddc
mirandaa <mirandaa@mpi-cbg.de>
2021-11-22 10:59:14 +0100

    fix: test.. and only eval a sample

--- tests/test_make_masterscan.py
+++ tests/test_make_masterscan.py
@@ -6,12 +6,10 @@ from x_masterscan import compareMasterScans
 from utils import expected_ms_path, make_masterscan, proy_path, read_options
 
 
-@pytest.mark.skip(
-    reason="incomplete recusrive pickeling, probably due to bad data struct. need to refact"
-)
 def test_make_masterscan():
     options = read_options(proy_path)
     masterscan = make_masterscan(options)
     expected = loadSC(expected_ms_path)
     expected.listSurveyEntry = expected.listSurveyEntry[:100]
+    masterscan.listSurveyEntry = masterscan.listSurveyEntry[:100]
     assert compareMasterScans(masterscan, expected)
