4685283dab3b74bce7b6eb447e71ff2ac5821e07
mirandaa <mirandaa@mpi-cbg.de>
2022-01-28 11:47:31 +0100

    test: mx2 masterescan with lx1 mfql

---
+++ test_resources/small_test/small_test_lx2_df.sc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f937bd2fa89a11d16c5e50df57645b304155292e1c315e801fee0fa0cd90637a
+size 1724646
--- tests/test_run_mfql.py
+++ tests/test_run_mfql.py
@@ -1,5 +1,6 @@
 import pickle
 import pytest
+from LX2_masterscan import make_lx2_masterscan
 
 from utils import (
     getMfqlFiles,
@@ -11,11 +12,12 @@ from utils import (
 )
 
 
-@pytest.mark.skip(
-    reason="other test change the masterscan, need to setup better, see conftest.py"
-)
+# @pytest.mark.skip(
+#     reason="other test change the masterscan, need to setup better, see conftest.py"
+# )
 def test_run_mfql():
     options = read_options(proy_path)
+
     with open(options["masterScanFileRun"], "rb") as handle:
         masterscan = pickle.load(handle)
     masterscan.listSurveyEntry = [
@@ -23,8 +25,31 @@ def test_run_mfql():
     ]
     for se in masterscan.listSurveyEntry:
         se.listMSMS = [msms for msms in se.listMSMS if 360 < msms.mass < 380]
+
     mfqlFiles = getMfqlFiles(r"test_resources\small_test")
+
     result = make_MFQL_result(masterscan, mfqlFiles, options)
     resultStr = makeResultsString(result, options)
 
     assert resultStr.strip() == out_reference.strip()
+
+
+def test_lx2_sc_on_run_mfql():
+    options = read_options(proy_path)
+
+    with open("test_resources/small_test/small_test.sc", "rb") as fh:
+        scan = pickle.load(fh)  # make_lx2_masterscan(options)
+
+    scan.listSurveyEntry = [
+        se for se in scan.listSurveyEntry if 600 < se.peakMean < 750
+    ]
+    for se in scan.listSurveyEntry:
+        se.listMSMS = [msms for msms in se.listMSMS if 360 < msms.mass < 380]
+
+    mfqlFiles = getMfqlFiles(r"test_resources\small_test")
+
+    result_lx2 = make_MFQL_result(scan, mfqlFiles, options)
+    resultStr_lx2 = makeResultsString(result_lx2, options)
+
+    assert resultStr_lx2.strip() == out_reference.strip()
+
