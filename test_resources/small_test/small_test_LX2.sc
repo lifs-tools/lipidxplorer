5fc2c3b27bdc314dc8658e67c44f5a2472711c65
mirandaa <mirandaa@mpi-cbg.de>
2022-02-11 17:43:51 +0100

    test: add lx2masterescan to test resources

---
+++ test_resources/small_test/small_test_LX2.sc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eda6534428adb87484e60e842b1520cb5f441263b9b7c4bf385ee46ee9dd3a5d
+size 7732190
--- tests/test_make_masterscan.py
+++ tests/test_make_masterscan.py
@@ -3,7 +3,13 @@ from lx.spectraImport import doImport, lpdxImportDEF_new
 from lx.spectraTools import loadSC
 from x_masterscan import compareMasterScans
 
-from utils import expected_ms_path, make_masterscan, proy_path, read_options
+from utils import (
+    expected_ms_path,
+    expected__lx2ms_path,
+    make_masterscan,
+    proy_path,
+    read_options,
+)
 from LX2_masterscan import make_lx2_masterscan
 
 # from memory_profiler import profile
@@ -21,6 +27,8 @@ def test_make_masterscan():
 def test_make_lx2_masterscan():
     options = read_options(proy_path)
     masterscan = make_lx2_masterscan(options)
-
-    assert False
+    expected = loadSC(expected_lx2ms_path)
+    expected.listSurveyEntry = expected.listSurveyEntry[:100]
+    masterscan.listSurveyEntry = masterscan.listSurveyEntry[:100]
+    assert compareMasterScans(masterscan, expected)
 
