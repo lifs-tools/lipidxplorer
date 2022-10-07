4668016f673fb374cad192c57de6ee8c6da7dbfd
mirandaa <mirandaa@mpi-cbg.de>
2022-10-07 15:06:59 +0200

    test: refactor directory structure

--- tests/test_t_sim.py
+++ tests/test_t_sim.py
@@ -1,52 +1,73 @@
 import pytest
-from lx.tools import op
-from utils import read_options
+from pathlib import Path
+from utils import read_options, make_MFQL_result, makeResultsString
 from LX1_masterscan import make_lx1_masterscan
 import pickle
 
 
 @pytest.fixture
 def get_options():
-    options = read_options(
-        r"test_resources\t_sim\t_sim_project\tSIM_Stitched.lxp"
-    )
+    options = read_options(r"test_resources\t_sim\project_data\tSIM_Stitched.lxp")
     return options
+
 
 @pytest.fixture
 def get_no_res_options():
-    options = read_options(
-        r"test_resources\t_sim\t_sim_project\tSIM_Stitched.lxp"
-    )
-    options['MSresolution'] = ''
-    options['MSMSresolution'] = ''
-    options['MSresolutionDelta'] = ''
-    options['MSMSresolutionDelta'] = ''
-
+    options = read_options(r"test_resources\t_sim\project_data\tSIM_Stitched.lxp")
+    options["MSresolution"] = ""
+    options["MSMSresolution"] = ""
+    options["MSresolutionDelta"] = ""
+    options["MSMSresolutionDelta"] = ""
     return options
 
-# def test_masterscan_by_lx1(get_options):
-#     masterscan = make_masterscan(get_options)
-#     assert False
 
-def test_masterscan_manual(get_options):
-    assert get_options['MSresolution']
-    masterscan = make_lx1_masterscan(get_options)
-    with open(r"test_resources\t_sim\reference_1.pkl", 'rb') as f: 
+@pytest.fixture
+def get_masterscan(get_options):
+    return make_lx1_masterscan(get_options)
+
+
+@pytest.fixture
+def get_no_res_masterscan(get_no_res_options):
+    return make_lx1_masterscan(get_no_res_options)
+
+
+@pytest.fixture
+def getMfqlFiles():
+    p = Path(r"test_resources\t_sim\mfqls")
+    mfqls = p.glob("*.mfql")
+    return {mfql.name: mfql.read_text() for mfql in mfqls}
+
+
+def test_masterscan_manual(get_options, get_masterscan):
+    assert get_options["MSresolution"]
+    masterscan = get_masterscan
+    with open(r"test_resources\t_sim\reference\masterscan_1.pkl", "rb") as f:
         reference = pickle.load(f)
-
     assert compareMasterScans(masterscan, reference)
 
-def test_masterescan_automatic(get_no_res_options):
-    assert not get_no_res_options['MSresolution']
 
-    masterscan = make_lx1_masterscan(get_no_res_options)
-    assert False
+def test_masterescan_automatic(get_no_res_options, get_no_res_masterscan):
+    assert not get_no_res_options._data["MSresolution"]
+    masterscan = get_no_res_masterscan
+    with open(r"test_resources\t_sim\reference\masterscan_2.pkl", "rb") as f:
+        reference = pickle.load(f)
+    assert compareMasterScans(masterscan, reference)
 
-def test_mfql_manual():
-    assert False
 
-def test_mfql_automatic():
-    assert False
+# def test_mfql_manual(get_options, getMfqlFiles):
+#     with open(r"test_resources\reference\masterscan_1.pkl", "rb") as f:
+#         get_masterscan = pickle.load(f)
+#     with open(r"test_resources\reference\resultStr_1.pkl", "rb") as f:
+#         reference = pickle.load(f)
+#     result = make_MFQL_result(get_masterscan, getMfqlFiles, get_options, log_steps=True)
+#     assert resultStr == reference
+
+
+# def test_mfql_automatic(getMfqlFiles, get_no_res_options, get_no_res_masterscan):
+#     # result = make_MFQL_result(get_no_res_masterscan, getMfqlFiles, get_no_res_options, log_steps=True)
+#     # resultStr = makeResultsString(result, get_no_res_options)
+#     assert False
+
 
 def compareMasterScans(created, reference):
     c_ls = created.listSurveyEntry
@@ -58,3 +79,4 @@ def compareMasterScans(created, reference):
             same = False
             break
     return same
+
