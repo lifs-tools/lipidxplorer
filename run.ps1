108c72fd8f69c2cc077715086a9e8042c67f05d3
HongKee Moon <moon@mpi-cbg.de>
2023-06-08 15:36:21 +0200

    chore: update test resources

--- run.ps1
+++ run.ps1
@@ -1,4 +1,2 @@
-$Remove = 'C:\Python27\Scripts;'
-$env:Path = ($env:Path.Split(';') | Where-Object -FilterScript {$_ -ne $Remove}) -join ';'
-$Remove = 'C:\Python27\;'
-$env:Path = ($env:Path.Split(';') | Where-Object -FilterScript {$_ -ne $Remove}) -join ';'
+$env:PATH = $env:PATH -replace "C:\\Python27\\Scripts;",""
+$env:PATH = $env:PATH -replace "C:\\Python27\\;",""
