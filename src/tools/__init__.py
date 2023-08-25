aa9ed332d6c66a02f209ec7a2c685f31ad731f54
HongKee Moon <moon@mpi-cbg.de>
2023-08-25 14:31:30 +0200

    chore: input the modules

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,2 +1,6 @@
 from .dump2out.dump2out import replace_results_with_min_abs_error
+from .peakStrainer.peakStrainerApp import openPeakStrainer
+from .reorderScans.reorderScans import openReorderScans
+from .simStitching.simStitcherApp import openSimStitcher
+from .simtrim.simtrimApp import openSimtrim
 from .sc2df import masterscan_2_df
