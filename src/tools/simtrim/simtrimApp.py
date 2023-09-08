03f872da3e6ac529f692937c27098e8b4a9e61bc
mirandaa <mirandaa@mpi-cbg.de>
2023-09-08 11:26:59 +0200

    feat: UI with wxformbuilder instead of Gooey
    gooey hasd some issues runnning

--- src/tools/simtrim/simtrimApp.py
+++ src/tools/simtrim/simtrimApp.py
@@ -1,85 +1,69 @@
-import os
-import signal
-import sys
-
-from tools.simtrim.simtrim import *
-from gooey import Gooey, GooeyParser
+import traceback
+import wx
+from standalone_resources.simtrim_form import simtrim_Frame
+from simtrim import simtrim
 
 
-@Gooey
-def main():
-    parser = GooeyParser(description="trim the sims from a spectra.")
-
-    parser.add_argument(
-        "-f",
-        "--files",
-        nargs="*",
-        help="spectra file with sims",
-        metavar="files",
-        widget="MultiFileChooser",
-    )
-
-    parser.add_argument(
-        "-d",
-        "--da",
-        metavar="custom dalton size to trim",
-        action="store",
-        type=float,
-        default=0,
-        help="trim these many daltons from sim, zero to calculate myself",
-    )
-
-    parser.add_argument(
-        "-s",
-        "--start",
-        metavar="start of retention time",
-        action="store",
-        type=float,
-        default=0,
-        help="time in minutes from where to start the simtrim",
-    )
-
-    parser.add_argument(
-        "-e",
-        "--end",
-        metavar="end of retention time",
-        action="store",
-        type=float,
-        default=float("inf"),
-        help="time in minutes where to end simtrim",
-    )
-
-    args = parser.parse_args()
-    for file in args.files:
+class simtrimControler(simtrim_Frame):
+    def __init__(self, parent):
+        super(simtrimControler, self).__init__(parent)
+	
+    def run_clicked( self, event ):
+        event.Skip()
+        file = self.m_filePicker10.GetPath()
         try:
-            simtrim(
-                file,
-                args.da,
-                start_rt=float(args.start),
-                stop_rt=float(args.end),
-            )
+            da = self.m_textCtrl3.GetValue()
+            da = float(da)
+            start_rt = self.m_textCtrl4.GetValue()
+            start_rt = float(start_rt)
+            stop_rt = self.m_textCtrl41.GetValue()
+            stop_rt = float(start_rt)
+
+            kwargs = {}
+            kwargs['start_rt'] = start_rt
+            kwargs['stop_rt'] = stop_rt
+
+            simtrim(file , da, **kwargs)
+
         except Exception as e:
-            print(f"Could not process {file}")
-            print(repr(e))
+            print(e)
+            print(traceback.print_exc())
+            dlg = wx.MessageDialog(
+                self,
+                "There was a problem processing the file {} \n{} \n{}".format(
+                    file, e, traceback.print_exc()
+                ),
+                "Warning",
+                wx.OK | wx.ICON_WARNING,
+            )
+            dlg.ShowModal()
+            dlg.Destroy()
+        
+        dlg = wx.MessageDialog(
+                self,
+                "Completed",
+                "Completed",
+                wx.OK ,
+            )
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    
+
+def openSimtrimControler(parent):
+    frame = simtrimControler(parent)
+    frame.Show(True)
+
+def main():
+    app = wx.App(False)
+    frame = simtrimControler(None)
+    frame.Show(True)
+    app.MainLoop()
+
+def openSimTrim(parent):
+    frame = simtrimControler(parent)
+    frame.Show(True)
 
 
-def simtrimTask():
-    print("simtrim")
-    import time
-    import subprocess as sp
-
-    sp.Popen(
-        "simtrimApp.py",
-        creationflags=sp.DETACHED_PROCESS | sp.CREATE_NEW_PROCESS_GROUP,
-        start_new_session=True,
-        stdout=sp.DEVNULL,
-        stderr=sp.STDOUT,
-    )
-
-
-def openSimtrim():
-    simtrimTask()
-
-
-if __name__ == "__main__" or __name__ == "tools.simtrim.simtrimApp":
+if __name__ == "__main__":
     main()
