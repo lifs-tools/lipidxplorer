6a01f25451bfd30022e714cc4a16bee5bdfd5340
HongKee Moon <moon@mpi-cbg.de>
2023-09-29 15:35:23 +0200

    chore: add auto resolution

--- run.ps1
+++ run.ps1
@@ -12,9 +12,9 @@ pip install attrdict3 requests
 
 # pip install "jax[cpu]" -f https://whls.blob.core.windows.net/unstable/index.html
 
-pip install git+https://github.com/mobiusklein/ms_deisotope
-
 pip install -r requirements.txt
+
+pip install --upgrade --force git+https://github.com/mobiusklein/ms_deisotope
 
 cp venv\lib\site-packages\pythonnet\runtime\Python.Runtime.dll venv\lib\site-packages\
 
